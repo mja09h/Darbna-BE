56f0cfe6fe2bec47209979e45b3b0d9a1c7f0e6d
Developer <mjat09@outlook.com>
2025-12-10 20:37:26 +0300

    mohammad

---
+++ src/apis/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import User from '../../models/Users';
+
+const getUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching users' });
+    }
+}
+
+export { getUsers };
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from './database';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
+connectDB();
 
 app.listen(Number(PORT), HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
